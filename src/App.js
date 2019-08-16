abb35cec171a017edf30f8ffe8095b9c76632c09
Aurélia <aurelia.h.vacher@gmail.com>
2019-08-16 23:08:51 +0300

    first commit for deployment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Jeruseo
+          Jeruseo - new site
         </a>
       </header>
     </div>
